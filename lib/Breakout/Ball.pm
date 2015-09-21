243f1d3816d711746e77b596a0c08d0adb40d667
Alan Berndt <alan@eatabrick.org>
2015-09-21 16:45:25 -0700

    Allow player to control direction with paddle

--- lib/Breakout/Ball.pm
+++ lib/Breakout/Ball.pm
@@ -49,9 +49,14 @@ sub update {
     if ($best) {
       $self->{x} += $dx * $best->{t};
       $self->{y} += $dy * $best->{t};
-      $self->{dir} = ($best->{dir} eq 'h' ? 3 : 2) * PI - $self->dir;
-
       $best->{object}->handle_collision($self);
+
+      if ($best->{object}->isa('Breakout::Paddle')) {
+        my $offset = ($self->x - $best->{object}->x) * 2 / $best->{object}->width;
+        $self->{dir} = (18 + 3 * $offset) * PI / 12;
+      } else {
+        $self->{dir} = ($best->{dir} eq 'h' ? 3 : 2) * PI - $self->dir;
+      }
 
       $step *= 1 - $best->{t};
     } else {
